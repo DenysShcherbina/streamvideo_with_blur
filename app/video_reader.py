27ef49dd42424a2d7de01fc5d5c66f9a37bee291
Denys Shcherbina <114299504+DenysShcherbina@users.noreply.github.com>
2026-08-15 12:20:17 +0200

    Update video_reader.py

--- app/video_reader.py
+++ app/video_reader.py
@@ -2,7 +2,7 @@ import cv2
 from kafka import KafkaConsumer
 from utils import decode_obj
 
-BOOTSTRAP_SERVERS = 'localhost:9092,localhost:9093,localhost:9094'
+BOOTSTRAP_SERVERS = 'localhost:9092,localhost:9093'
 VIDEO_TOPIC = 'videostream_out'
 
 consumer = KafkaConsumer(VIDEO_TOPIC,
